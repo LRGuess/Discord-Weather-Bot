3dd478c1064cc57006c4ebf8bb1739227f400c94
Liam <95448250+LRGuess@users.noreply.github.com>
2024-06-08 22:26:14 -0600

    Pord binding for Render!

--- Discord Weatherbot/mainBot.py
+++ Discord Weatherbot/mainBot.py
@@ -17,6 +17,27 @@ import requests
 import datetime
 from dotenv import load_dotenv
 import os
+import socket
+import sys
+ 
+HOST = '' 
+PORT = 5555 
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((HOST, PORT))
+    
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+	
+print('Socket bind complete')
+s.listen(10)
+
+conn, addr = s.accept()
+
+print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
 
 # Load environment variables from .env file
 load_dotenv()
