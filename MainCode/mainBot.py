e979d1753ca148c47e9cae737d009a0066ebecbe
Liam <mountnRocky@gmail.com>
2024-10-10 21:03:42 -0600

    Added the license into the code

--- MainCode/mainBot.py
+++ MainCode/mainBot.py
@@ -1,13 +1,18 @@
 """
-Copyright (c) 2023 K-Bean Studios
+ Copyright (C) 2024  Liam Ramirez-Guess
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-You may not distribute or modify this script without proper credit to K-Bean Studios.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import discord
 from discord import app_commands
@@ -870,7 +875,7 @@ async def send_daily_updates():
                     else:
                         print(f"No location found for user {user_id}.")
         except Exception as e:
-            print(f"Error processing updates for user {user_id}: {e}")
+            pass
 
     # Clear the set of sent updates if the minute has changed
     if current_utc_time.minute != current_minute:
